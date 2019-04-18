c245b47f801f7db5a0d4ec1c7e031fe92a88b3fb
zain58 <zainnoman52@gmail.com>
2019-04-18 11:41:03 +0500

    second

--- web/Newsblogs/a.py
+++ web/Newsblogs/a.py
@@ -1,2 +1,2 @@
 import os
-os.rename('E:\\work fyp\\final crawler\\venv\\AryTemp.csv', 'E:\\work fyp\\final crawler\\venv\\ary.csv')
+os.rename('E:\\work fyp\\final crawler\\venv\\NewsOnetemp.csv', 'E:\\work fyp\\final crawler\\venv\\news.csv')
