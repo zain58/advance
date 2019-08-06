510bb3d4550a7c2203c5055025a611a839c968d7
zain58 <zainnoman52@gmail.com>
2019-08-06 10:58:47 +0500

    made changes

--- shahhahaha.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-d = pd.read_csv("E:/FYP/NewsOne/News-One.csv", encoding='ISO-8859-1')
-e = pd.read_csv("E:/FYP/NewsOne/opinion.csv", encoding='ISO-8859-1')
-
-f = pd.concat([d, e], axis=1)
-f.to_csv("E:/FYP/NewsOne/Final-NewsOne.csv", index=False, encoding='utf-8')
